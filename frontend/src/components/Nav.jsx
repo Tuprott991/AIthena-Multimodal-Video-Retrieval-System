58172a7ed80409fd4d1e07792ed3580ee9a72500
Tuprott991 <nvtu22@clc.fitus.edu.vn>
2024-10-18 16:46:45 +0700

    Update logo

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -9,10 +9,12 @@ const Nav = () => {
  return (
   <nav className="bg-white border-b border-gray-200 px-4 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50 flex">
    <div
-    className="cursor-pointer self-center text-xl font-semibold whitespace-nowrap dark:text-white mr-8"
+    className="cursor-pointer self-center text-xl font-bold whitespace-nowrap dark:text-white mr-8"
     onClick={handleClick}
+    style={{ fontFamily: "'Montserrat', 'Poppins', 'Raleway', sans-serif" }}
    >
-    AIthena
+    <span style={{ color: "orange" }}>AI</span>
+    <span style={{ color: "limegreen" }}>thena</span>
    </div>
    <HeaderSearch></HeaderSearch>
   </nav>
